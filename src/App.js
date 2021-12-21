cbbdbbe0ddcb5f15e8782d996817b0371a81d005
inspiringsource <ab.b2k3@gmail.com>
2021-12-21 17:09:41 +0100

    final step

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
+
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-/*
-Challenge: Pass props to the Card component and display that data
+import data from "./data"
 
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
 
-*/
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+      key={item.id}
+      {...item}
+  />
+    )
+})   
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-            imgName="katie-zaferes.png"
-            rating="5.0"
-            reviewCount="(6)"
-            country="USA"
-            title="Life Lessons with Katie Zaferes"
-            price="136"
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -19,18 +19,25 @@ Notes:
 */
 
 export default function Card(props) {
-  console.log(props)
-    return (
-      <div className='card'>
-        <img className='card--image' src={require(`../images/${props.imgName}`)} alt='Katie Zaferes' ></img>
-        <div className='card--stats'>
-          <img src={star} alt='Star rating'></img>
-          <span>{props.rating}</span>
-          <span className='gray'>{props.reviewCount}</span>
-          <span className='gray'> • {props.country}</span>
-        </div>
-        <p>{props.title}</p>
-        <p><span className='bold'>From $ {props.price}</span> / person</p>
+  let badgeText
+  if (props.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+      badgeText = "ONLINE"
+  }
+  
+  return (
+      <div className="card">
+          {badgeText && <div className="card--badge">{badgeText}</div>}
+          <img src={`../images/${props.coverImg}`} className="card--image" />
+          <div className="card--stats">
+              <img src="../images/star.png" className="card--star" />
+              <span>{props.stats.rating}</span>
+              <span className="gray">({props.stats.reviewCount}) • </span>
+              <span className="gray">{props.location}</span>
+          </div>
+          <p className="card--title">{props.title}</p>
+          <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
       </div>
     )
 }
